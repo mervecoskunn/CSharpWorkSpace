f581c69863345b0e1fb7c87d0e07741df22f14cb
merve <merve@LAPTOP-POAOTUBR>
2022-02-18 09:37:15 +0100

    Add project files.

---
+++ VolumeOfCyclinder/VolumeOfCyclinder.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VolumeOfCyclinder
+{
+    internal class VolumeOfCyclinder
+    {
+        static void Main(string[] args)
+        {
+            //(Compute the volume of a cylinder) Write a program that reads in the radius
+            //and length of a cylinder and computes the area and volume using the following
+            // formulas:
+            //area = radius * radius * p
+            //volume = area * length
+
+            
+            Console.WriteLine("Welcome to volume calculator");
+            Console.Write("Enter radius: ");
+            double r;
+            Double.TryParse(Console.ReadLine(), out r);
+            Console.Write("Enter length: ");
+            double l;
+            Double.TryParse(Console.ReadLine(),out l);
+            double p = 3.14;
+            double area = r*r*p;
+            Console.WriteLine("Area: " +area);
+
+            double volume = area * l;
+            Console.WriteLine("Volume: "+ volume);
+
+
+
+
+
+            Console.ReadLine();
+
+
+        }
+    }
+}
