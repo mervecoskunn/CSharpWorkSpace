f5bd36593bbe2395fcdb854dcafd62c356ba3cb2
Merve Coskun <merve_coskun2013@hotmail.com>
2022-02-23 12:52:11 +0100

    The result of the product of negative and positive numbers.

---
+++ Ch_3_Homework2/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ch_3_Homework2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var a = 10;
+            var b = 20;
+            var c = 30;
+            if (a < 0)
+                Console.WriteLine("a*b*c= " + "The result will be negative.");
+            if (b < 0)
+                    Console.WriteLine("a*b*c= " + "The result will be negative.");
+            if (c < 0)
+                        Console.WriteLine("a*b*c= "+ "The result will be negative.");
+            if (a > 0 && b > 0 && c > 0)
+                Console.WriteLine("a*b*c= "+ "The result will be positive.");
+
+            Console.ReadLine();
+                
+            
+
+
+            
+        }
+    }
+}
