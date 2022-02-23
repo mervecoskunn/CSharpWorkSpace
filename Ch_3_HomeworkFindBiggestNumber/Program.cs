daf9d477b2b66203de3b54cb18f843d2d7b2dc2e
Merve Coskun <merve_coskun2013@hotmail.com>
2022-02-23 12:59:44 +0100

    Find biggest number.

---
+++ Ch_3_HomeworkFindBiggestNumber/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ch_3_HomeworkFindBiggestNumber
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = 30;
+            int b = 20;
+            int c = 10;
+            if (a>b)
+            {
+                Console.WriteLine("a bigger than b.");
+                if (b>c)
+                {
+                    Console.WriteLine("a is also  biggest number.");
+                }
+                Console.ReadLine();
+            }
+        }
+    }
+}
