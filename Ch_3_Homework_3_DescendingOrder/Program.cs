3e6ae1132562002bca6d108adf84760b3e8d1894
Merve Coskun <merve_coskun2013@hotmail.com>
2022-02-23 13:30:43 +0100

    Show the digits as a word.

---
+++ Ch_3_Homework_3_DescendingOrder/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ch_3_Homework_3_DescendingOrder
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = 10;
+            int b = 20;
+            int c = 30;
+            if (a<b)
+            {
+                Console.WriteLine("a smaller than b.");
+                if (b<c)
+                {
+                    Console.WriteLine("b smaller than c."+"\na is also smallest number.");
+                    
+                }
+                Console.ReadLine();
+            }
+        }
+    }
+}
---
+++ Ch_3_Homework_4_ShowTheDigitsAsAWord/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ch_3_Homework_4_ShowTheDigitsAsAWord
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int number;
+            Console.Write("Enter a number between 0 and 9: ");
+            int.TryParse(Console.ReadLine(), out  number);
+            if (number == 0)
+                Console.WriteLine("The number is zero");
+            if (number == 1)
+                Console.WriteLine("The number is one");
+            if (number == 2)
+                Console.WriteLine("The number is two.");
+            if (number == 3)
+                Console.WriteLine("The number is three.");
+            if (number == 4)
+                Console.WriteLine("The number is four.");
+            if (number == 5)
+                Console.WriteLine("The number is five.");
+            if (number == 6)
+                Console.WriteLine("The number is six.");
+            if (number == 7)
+                Console.WriteLine("The number is seven.");
+            if (number == 8)
+                Console.WriteLine("The number is eight.");
+            if (number == 9)
+                Console.WriteLine("The number is nine.");
+            Console.ReadLine();
+            
+
+
+
+
+
+
+
+        }
+    }
+}
