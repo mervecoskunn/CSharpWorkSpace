41c7cd1af8acfa2bd5a755acabbcac009b0a31ce
Merve Coskun <merve_coskun2013@hotmail.com>
2022-02-28 13:21:59 +0100

    While Loop Exercise 1

---
+++ whileLoopExercise1/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace whileLoopExercise1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // from 1 to 10 digits displayed
+            int i = 0;
+            while (i < 10)
+            {
+                Console.WriteLine(i);
+                i++;
+            }
+            Console.ReadLine();
+        }
+    }
+}
