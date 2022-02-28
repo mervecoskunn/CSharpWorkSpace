1c16fbb225fe946086b2ca9598eb6226ef6664aa
Merve Coskun <merve_coskun2013@hotmail.com>
2022-02-28 13:53:22 +0100

    sum of digits(from1 to 10)

---
+++ whileLoopsExercise2/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace whileLoopsExercise2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // Sum of the digits(from 1 to 10)
+            int i = 0;
+            int n = 10;
+            int sum = 0;
+            while (i<=n)
+            {
+                sum += i;
+                i++;
+
+            }
+            Console.WriteLine("Sum: "+ sum);
+            Console.ReadLine();
+            
+
+            
+        }
+    }
+}
