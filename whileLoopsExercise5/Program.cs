e378118801a5ce59434b731fa9bddaf6150806e2
Merve Coskun <merve_coskun2013@hotmail.com>
2022-02-28 15:57:09 +0100

    collection quiz

---
+++ whileLoopsExercise5/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace whileLoopsExercise5
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int questionNumbers = 5;
+            int correctAnswer = 0;
+            int n1, n2;
+            Random random =new Random();
+            int answer;
+            int currentQuestion = 1;
+            while (currentQuestion<=questionNumbers)
+            {
+                n1 = random.Next(10);  
+                n2 = random.Next(10);
+                Console.Write(currentQuestion + ") " + n1 + " + "+ n2 + " = ");
+                Int32.TryParse(Console.ReadLine(), out answer);
+                if (answer==(n1+n2))
+                    correctAnswer++;
+                currentQuestion++;
+                
+
+
+
+            }
+            Console.WriteLine("you have entered " + correctAnswer + " correct answers.");
+            Console.ReadLine();
+
+
+        }
+    }
+}
