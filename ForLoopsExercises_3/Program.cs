eb464fe24b91f9b26e60f33bf15b373f59317803
Merve Coskun <merve_coskun2013@hotmail.com>
2022-03-14 11:58:49 +0100

    .

---
+++ ForLoopsExercises_3/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ForLoopsExercises_3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string name;
+            int totalName;
+            
+            for (int i = 1; i <=10 ; i++)
+            {
+                Console.Write("Enter your name :");
+                name = Convert.ToString(Console.ReadLine());
+                Console.WriteLine("{0}:{1}",i,name);
+
+            }
+            Console.ReadLine();
+            
+        }
+    }
+        
+}
