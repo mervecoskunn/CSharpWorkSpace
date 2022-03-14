339144fb190617f70b956eb922ce520fafab6fc6
Merve Coskun <merve_coskun2013@hotmail.com>
2022-03-14 11:41:15 +0100

    all letters

---
+++ ForLoopExerciseExstra_2/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ForLoopExerciseExstra_2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //from a until z 
+            char i;
+            for ( i = 'a'; i <='z'; i++)
+            {
+                Console.WriteLine( i + " ");
+
+            }
+            Console.ReadLine();
+            
+          
+           
+        }
+    }
+}
