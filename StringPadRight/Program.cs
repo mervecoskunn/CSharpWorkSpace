7d267bea1cf55dff9c45a4ee81ce0e3dffce39a1
Merve Coskun <merve_coskun2013@hotmail.com>
2022-03-19 20:27:19 +0100

    String PadRight

---
+++ StringPadRight/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StringPadRight
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //String PadRight
+            string str1 = "Merve";
+            string str2 = "";
+            int totalLength = 8;
+            int i = 0;
+            char c = '?';
+            int addCharacterLength = totalLength - str1.Length;
+            
+            if (str1.Length<totalLength)
+            {
+                for ( i = 0; i < addCharacterLength+1; i++)
+                    str2+= c;
+                Console.WriteLine("New string: "+ str1 + str2);
+            }
+            Console.ReadLine();
+
+
+        }
+    }
+}
