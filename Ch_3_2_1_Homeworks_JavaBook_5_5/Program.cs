b94e46df4060895e896b46056a3ac816d2387e49
Merve Coskun <merve_coskun2013@hotmail.com>
2022-03-24 15:44:46 +0100

    Conversion from kilograms to pounds and pounds to kilograms

---
+++ Ch_3_2_1_Homeworks_JavaBook_5_5/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ch_3_2_1_Homeworks_JavaBook_5_5
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            /* (Conversion from kilograms to pounds and pounds to kilograms) Write a program
+               that displays the following two tables side by side:
+             */
+            Console.WriteLine("Kilograms\tPounds\t\tPounds\tKilograms");
+            double pound;
+            double kilogram;
+            double poundPerKilogram = 2.2;
+            for (int  k = 1 ,  p =20; k <=199 && p<=515; k+=2,p+=5)
+                {
+                
+                pound = k * poundPerKilogram;
+                kilogram = p / 2.2;
+                Console.WriteLine(k+"\t\t"+ pound+"\t|\t"+p+"\t"+kilogram);
+            }
+
+            Console.ReadLine();
+
+
+
+
+
+        }
+    }
+}
