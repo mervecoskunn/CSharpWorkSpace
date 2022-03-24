87f445acffe97a40dc293c721f604077d5fb52de
Merve Coskun <merve_coskun2013@hotmail.com>
2022-03-24 21:59:40 +0100

    Find the smallest n such that n2 > 12,000

---
+++ Ch_3_2_1_Homeworks_JavaBook_5_11/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ch_3_2_1_Homeworks_JavaBook_5_11
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+           
+                /*(Find numbers divisible by 5 or 6, but not both) Write a program that displays
+                   all the numbers from 100 to 200, ten per line, that are divisible by 5 or 6, but not
+                   both.Numbers are separated by exactly one space.*/
+
+            int counter=0;
+            int perLineCount = 10;
+            
+            for (int i = 100; i <=200; i++)
+            {
+                if (i % 5 == 0 || i % 6 == 0)
+                {
+                    counter++;
+                    if (counter % perLineCount == 0)
+                        Console.WriteLine(i);
+                    else
+                        Console.Write(i + " ");
+                }
+            }
+            Console.ReadLine();
+        }
+    }
+}
---
+++ Ch_3_2_1_Homeworks_JavaBook_5_12/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ch_3_2_1_Homeworks_JavaBook_5_12
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            /*
+             * (Find the smallest n such that n2 > 12,000) Use a while loop to find the smallest 
+                integer n such that n2 is greater than 12,000.
+             * 
+             */
+            int n = 0;
+
+            while (Math.Pow(n, 2) < 12000)
+            {
+                n++;
+            }
+            Console.WriteLine(n + " is the lowest number, such that n^2 is greater than 12,000");
+            Console.WriteLine("Lowest number: " + (n - 1) + "^2 = " + Math.Pow(n - 1, 2));
+            Console.WriteLine("Greater than (12.000): " + n + "^2 = " + Math.Pow(n, 2));
+
+            Console.ReadLine();
+        }
+        
+    }
+}
