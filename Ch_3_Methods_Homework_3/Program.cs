563e23bc0bb14c6ae7274c2945d3e18033c366aa
Merve Coskun <merve_coskun2013@hotmail.com>
2022-04-19 09:52:59 +0200

    method for sum of odds numbers from 1 until n

---
+++ Ch_3_Methods_Homework_3/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ch_3_Methods_Homework_3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //method for sum of odds numbers from 1 until n
+            int total = sumOddsUntil(10);
+            Console.WriteLine("Sum odds numbers from 1 until 10: "+ total);
+            Console.ReadLine();
+
+        }
+        public static int sumOddsUntil(int n )
+        {
+            int sum = 0;
+               n = 10;
+            for (int i = 1; i < n; i+=2)
+            {
+                sum += i;
+
+            }
+            return sum;
+
+        }
+    }
+}
