80f8f604a1842e6f274fabd2e06c8e764f57544c
Merve Coskun <merve_coskun2013@hotmail.com>
2022-04-19 09:46:25 +0200

    (Method) sum of even numbers from 1 until n

---
+++ Ch_3_Methods_Homework_2/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ch_3_Methods_Homework_2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // sum of even numbers from 1 until n 
+            int total = SumEvensUntil(10);
+            Console.WriteLine("Sum of evens number from 1 until 10: "+total);
+            Console.ReadLine();
+
+
+        }
+        public static int SumEvensUntil(int n)
+        {
+            int sum = 0;
+            n = 10;
+            for (int i = 2; i < 10; i += 2)
+            {
+                sum += i;
+            }
+            return sum;
+        }
+    }
+}
