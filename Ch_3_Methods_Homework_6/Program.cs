2ae659e04ae4b4cc43502c62cb319caa25614920
Merve Coskun <merve_coskun2013@hotmail.com>
2022-04-19 10:53:38 +0200

    write 3 integer numbers

---
+++ Ch_3_Methods_Homework_6/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ch_3_Methods_Homework_6
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //write 3 integer numbers
+
+            WriteThreeInteger(10,20,30);
+            Console.ReadLine();
+        }
+        public static void WriteThreeInteger(int number1, int number2, int number3)
+        {
+            Console.WriteLine("Number 1: "+number1);
+            Console.WriteLine("Number 2: " + number2);
+            Console.WriteLine("Number 3: " + number3);
+            Console.ReadLine();
+        }
+    }
+}
