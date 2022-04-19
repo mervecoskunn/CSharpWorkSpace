dde51cea08a7612d580c04589ef0158a214f5465
Merve Coskun <merve_coskun2013@hotmail.com>
2022-04-19 10:25:13 +0200

    method for BMI

---
+++ Ch_3_Methods_Homework_4/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ch_3_Methods_Homework_4
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // method for random months
+        }
+        public static void RandomMonth()
+        {
+            Random month = new Random();
+           
+        }
+    }
+}
---
+++ Ch_3_Methods_Homework_5/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ch_3_Methods_Homework_5
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //method for BMI
+            Console.Write("Enter your weight(kg): ");
+            double weight;
+            double.TryParse(Console.ReadLine(), out weight);
+            Console.Write("Enter your height(m): ");
+            double height;
+            double.TryParse(Console.ReadLine(), out height);
+            double BMI=CalculateBMI(weight, height);
+            if (BMI < 18.5)
+            {
+                Console.WriteLine("BMI is "+ Math.Round(BMI, 2) + " Underweight");
+            }
+            else if (BMI < 25)
+            {
+                Console.WriteLine("BMI is " + Math.Round(BMI, 2) + " Normal");
+            }
+            else if (BMI < 30)
+            {
+                Console.WriteLine("BMI is " + Math.Round(BMI, 2) + " Overweight");
+            }
+            else
+            {
+                Console.WriteLine("BMI is " + Math.Round(BMI, 2) + " Obese");
+            }
+
+            Console.ReadLine();
+
+
+        }
+        public static double CalculateBMI(double weight, double height)
+        {
+            double BMI = weight/(height*height);
+            return BMI;
+
+        }
+    }
+}
